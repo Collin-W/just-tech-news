1d64fd254a923e46ac28c82eb6edce1ea4d67e3c
Collin-W <whalencollin@gmail.com>
2021-11-15 14:33:51 -0600

    user post routes created, users can vote on posts

--- models/index.js
+++ models/index.js
@@ -1,6 +1,47 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
+
+// create associations how the models connect to one another
+// this is a one to many relationship, 1 user can make many posts
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+// reverse association like having require and export
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+  });
+  
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+  });
+
+  Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+  Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+  });
+  
+  User.hasMany(Vote, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+  });
 
 
 
-module.exports = { User, Post };
+
+module.exports = { User, Post, Vote };
